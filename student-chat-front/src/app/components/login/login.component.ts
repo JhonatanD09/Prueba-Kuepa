4d7b56e3986c2150d87f2db0a1169a5ad9f99a75
JhonatanD09 <JORANADA@GMAIL.COM>
2024-02-16 15:54:08 -0500

    Create project and login front

---
+++ student-chat-front/src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+
+  constructor(private router :Router) {}
+
+  login() {
+    console.log(this.email);
+    console.log(this.password);
+    this.router.navigateByUrl('chat')
+  }
+}
---
+++ student-chat-front/src/app/components/student-chat/student-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-chat.component.html',
+  styleUrl: './student-chat.component.css'
+})
+export class StudentChatComponent {
+
+}
